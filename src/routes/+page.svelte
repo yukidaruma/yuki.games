989181de823acd77d8a8a74a6ac0f80c011a22df
yukidaruma <yukidaruma@users.noreply.github.com>
2025-03-03 17:20:06 +0900

    feat: Add data to "Personal Projects" section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,12 @@
 	<div>
 		<GitHubProjects />
 
-		<p class="mt-4">
-			For all projects including minor ones, please visit "<a
+		<p class="mt-4 text-sm text-slate-400">
+			For published projects not on this list, please visit "<a
 				href="https://github.com/yukidaruma?tab=repositories&type=source">Repositories</a
 			>" tab on my GitHub profile.
 		</p>
-		<p class="mt-2">
+		<p class="text-sm text-slate-400">
 			You can also find OSS contributions at
 			<a
 				href="https://github.com/issues?q=author%3Ayukidaruma+is%3Aissue+is%3Apublic+-user%3Ayukidaruma"
@@ -25,7 +25,7 @@
 			<a href="https://github.com/pulls?q=author%3Ayukidaruma+is%3Apublic+-user%3Ayukidaruma">
 				Pull Requests
 			</a>
-			on GitHub.
+			search on GitHub.
 		</p>
 	</div>
 </section>
@@ -38,7 +38,7 @@
 				<a href="https://github.com/yukidaruma">GitHub</a>
 			</li>
 			<li>
-				<a href="https://twitter.com/YukiDotGames">X</a> (Formerly Twitter)
+				<a href="https://twitter.com/YukiDotGames">X</a>
 			</li>
 		</ul>
 	</div>
