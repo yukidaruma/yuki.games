744d0ba1b906c0522a05f9e86d27576d06467f3c
yukidaruma <yukidaruma@users.noreply.github.com>
2025-09-25 01:40:11 +0900

    chore: Update Privacy Policy to mention distribution platforms

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -6,6 +6,11 @@
 			Unless otherwise stated, all of my websites and apps contain no analytics, telemetry, or
 			tracking code.
 		</p>
-		<p>Please note that some of my websites (including yuki.games) use Cloudflare for hosting.</p>
+		<p>
+			Please note that some of my websites (including yuki.games) use external hosting services,
+			which may have their own privacy policies. For example, this website (yuki.games) uses Vercel
+			for hosting. Some of my projects also use platforms like GitHub or Chrome Web Store for
+			distribution. Please refer to their respective privacy policies for more information.
+		</p>
 	</div>
 </section>
