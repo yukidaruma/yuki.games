9d317a0efdd54edbbd1947995b62df6cb6d8ab86
yukidaruma <yukidaruma@users.noreply.github.com>
2025-09-25 20:00:46 +0900

    feat: Apply color theme to syntax highlight

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -34,6 +34,7 @@
 
 	:global {
 		@import 'github-markdown-css/github-markdown-dark.css';
+		@import 'prism-themes/themes/prism-a11y-dark.css';
 
 		.toc {
 			@apply pb-6 mb-6 border-b-4 border-[#3d444d];
@@ -48,6 +49,24 @@
 		.markdown-body {
 			ol {
 				@apply list-decimal;
+			}
+
+			/* Show class name on code blocks as language name */
+			pre[class*='language-']:not(.language-undefined)::before {
+				content: attr(class);
+				position: absolute;
+				top: 0.5rem;
+				right: 0.5rem;
+				font-size: 0.75rem;
+				font-weight: bold;
+				color: #6b7280;
+				text-transform: none;
+				background: rgba(0, 0, 0, 0.1);
+				padding: 0.25rem 0.5rem;
+				border-radius: 0.25rem;
+			}
+			pre[class*='language-'] {
+				position: relative;
 			}
 		}
 	}
