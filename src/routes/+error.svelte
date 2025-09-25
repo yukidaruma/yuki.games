abd198f0b4a8dbaeac26d00805a005459c56e8de
yukidaruma <yukidaruma@users.noreply.github.com>
2025-09-25 09:15:13 +0900

    Add error page
    There is special snow-themed message for 404 error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	const message: Record<number, string> = {
+		400: 'Bad Request',
+		404: 'Not Found',
+		500: 'Internal Server Error',
+		503: 'Service Unavailable'
+	} as const;
+</script>
+
+<h2>
+	Error {page.status}
+	{#if page.error.message}
+		({page.error.message})
+	{:else if message[page.status]}
+		({message[page.status]})
+	{/if}
+</h2>
+<section>
+	<div>
+		{#if page.status == 404}
+			<p>The snowman seems to have melted away... 🫠</p>
+		{/if}
+	</div>
+</section>
