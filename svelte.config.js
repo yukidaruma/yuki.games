e0e8306f826740154c2ee4446db09ac8f14e280c
yukidaruma <yukidaruma@users.noreply.github.com>
2025-09-28 10:38:31 +0900

    feat(blog): Remove .md extensions from Markdown links

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import toc from '@jsdevtools/rehype-toc';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import rehypeUrls from 'rehype-urls';
 import remarkFootnotes from 'remark-footnotes';
 import remarkHeadingId from 'remark-heading-id';
 
@@ -28,7 +29,16 @@ export default {
 							]
 						};
 					}
-				}
+				},
+				[
+					rehypeUrls,
+					function (url) {
+						if (url.pathname?.endsWith('.md')) {
+							url.pathname = url.pathname.slice(0, -3);
+						}
+						return url;
+					}
+				]
 			]
 		})
 	],
